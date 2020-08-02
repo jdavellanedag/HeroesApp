d6b226be521be27cc220a968b4c70adb12ac6344
jdavellanedag <juldave.gal@hotmail.com>
2020-08-02 16:45:38 -0500

    Search module added

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,10 +1,26 @@
-import React from "react";
-import { heroes } from "../../data/Heroes";
+import React, { useMemo } from "react";
+import queryString from "query-string";
+import { useLocation } from "react-router-dom";
 import { HeroCard } from "../heroes/HeroCard";
+import { useForm } from "../../hooks/useForm";
+import { getHeroesByName } from "../../selectors/getHeroesByName";
 
-export const SearchScreen = () => {
-  const heroesFilter = heroes;
-  const handleSearch = () => {};
+export const SearchScreen = ({ history }) => {
+  const location = useLocation();
+  const { q = "" } = queryString.parse(location.search);
+
+  const [formValues, handleInputChange] = useForm({
+    searchText: q,
+  });
+  const { searchText } = formValues;
+
+  const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    history.push(`?q=${searchText}`);
+  };
+
   return (
     <div>
       <h1>Search</h1>
@@ -13,22 +29,34 @@ export const SearchScreen = () => {
         <div className="col-5">
           <h4>Search Form</h4>
           <hr />
-          <form>
+          <form onSubmit={handleSearch}>
             <input
               type="text"
               placeholder="Find your hero"
               className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
             ></input>
             <button
               type="submit"
               className="btn m-1 btn-block btn-outline-primary"
-            ></button>
+            >
+              Buscar
+            </button>
           </form>
         </div>
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          {heroesFilter.map((hero) => (
+          {q === "" && <div className="alert alert-info">Buscar un heroe</div>}
+          {q !== "" && heroesFiltered.length === 0 && (
+            <div className="alert alert-danger">
+              No hay heroes con el nomber {q}
+            </div>
+          )}
+          {heroesFiltered.map((hero) => (
             <HeroCard key={hero.id} {...hero} />
           ))}
         </div>
